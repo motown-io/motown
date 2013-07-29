f52cd81aa3bd098f50d4227c5f2d7c56340b6ef2
Dennis Laumen <dennis.laumen@ihomer.nl>
2013-07-29 16:06:13 +0200

    Removed unused imports from UnlockConnectorJsonCommandHandler

--- operator-api/json/src/main/java/io/motown/operatorapi/json/commands/UnlockConnectorJsonCommandHandler.java
+++ operator-api/json/src/main/java/io/motown/operatorapi/json/commands/UnlockConnectorJsonCommandHandler.java
@@ -5,8 +5,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.motown.domain.api.chargingstation.ChargingStationId;
 import io.motown.domain.api.chargingstation.RequestUnlockConnectorCommand;
-import io.motown.operatorapi.json.commands.DomainCommandGateway;
-import io.motown.operatorapi.json.commands.JsonCommandHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
