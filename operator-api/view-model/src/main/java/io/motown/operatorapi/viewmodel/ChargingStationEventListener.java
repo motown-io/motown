a29bf07cc1904717487a98e4c281c37db83c4fa7
Dennis Laumen <dennis.laumen@ihomer.nl>
2013-08-07 19:17:16 +0200

    Cleaned up unused imports

--- operator-api/view-model/src/main/java/io/motown/operatorapi/viewmodel/ChargingStationEventListener.java
+++ operator-api/view-model/src/main/java/io/motown/operatorapi/viewmodel/ChargingStationEventListener.java
@@ -9,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 @Component
 public class ChargingStationEventListener {
 
