a1ea27102b29a10c8d5f7f262d5bd48e41b37756
Mark van den Bergh <mark.van.den.bergh@ihomer.nl>
2013-11-21 16:15:14 +0100

    Continuation prevents transport thread blocking By making use of CXF continuation we prevent that the transport thread gets blocked while we wait for response on the bootNotification command.

--- ocpp/soap/src/main/java/io.motown.ocpp.soap/centralsystem/v1_5/CentralSystemService.java
+++ ocpp/soap/src/main/java/io.motown.ocpp.soap/centralsystem/v1_5/CentralSystemService.java
@@ -21,8 +21,20 @@ import io.motown.ocpp.soap.util.DateConverter;
 import io.motown.ocpp.viewmodel.ChargingStationSubscriber;
 import io.motown.ocpp.viewmodel.domain.DomainService;
 import io.motown.ocpp.viewmodel.domain.BootChargingStationResult;
+import org.apache.cxf.continuations.Continuation;
+import org.apache.cxf.continuations.ContinuationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.task.TaskExecutor;
+
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 @javax.jws.WebService(
         serviceName = "CentralSystemService",
@@ -32,13 +44,30 @@ import org.springframework.beans.factory.annotation.Qualifier;
         endpointInterface = "io.motown.ocpp.soap.centralsystem.v1_5.schema.CentralSystemService")
 public class CentralSystemService implements io.motown.ocpp.soap.centralsystem.v1_5.schema.CentralSystemService {
 
+    private static final Logger log = LoggerFactory.getLogger(CentralSystemService.class);
+
+    //TODO make this configurable
+    /**
+     * Timeout in milliseconds for the continuation suspend functionality
+     */
+    private final int CONTINUATION_TIMEOUT = 5000;
+
+    //TODO make this configurable
+    /**
+     * Heartbeat interval which will be returned to the client if handling the bootNotification failed
+     */
+    private final int HEARTBEAT_INTERVAL_FALLBACK = 900;
+
     @Autowired
     private DomainService domainService;
 
     @Autowired
-    private ObjectFactory objectFactory;
+    private TaskExecutor taskExecutor;
 
-    // not using @Resource(name = "axonAmqpChargingStationSubscriber") as this causes startup issues on Glassfish. JNDI lookups vs Spring.
+    @Resource
+    private WebServiceContext context;
+
+    // not using @Resource(name = "axonAmqpChargingStationSubscriber") for this bean as it causes startup issues on Glassfish. JNDI lookups vs Spring.
     @Autowired
     @Qualifier("axonAmqpChargingStationSubscriber")
     private ChargingStationSubscriber chargingStationSubscriber;
@@ -46,71 +75,139 @@ public class CentralSystemService implements io.motown.ocpp.soap.centralsystem.v
     @Override
     public DataTransferResponse dataTransfer(DataTransferRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [dataTransfer] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public StatusNotificationResponse statusNotification(StatusNotificationRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [statusNotification] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public StopTransactionResponse stopTransaction(StopTransactionRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [stopTransaction] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
-    public BootNotificationResponse bootNotification(BootNotificationRequest parameters, String chargeBoxIdentity) {
-        ChargingStationId chargingStationId = new ChargingStationId(chargeBoxIdentity);
+    public BootNotificationResponse bootNotification(final BootNotificationRequest parameters, final String chargeBoxIdentity) {
+        final Continuation continuation = getContinuation();
 
-        chargingStationSubscriber.subscribe(chargingStationId);
+        if (continuation == null) {
+            log.error("Failed to get continuation, falling back to synchronous request handling. Make sure async-supported is set to true on the CXF servlet (web.xml)");
+            //TODO implement fallback - Mark van den Bergh, November 21st 2013
+            throw new RuntimeException("Failed to get continuation");
+        }
 
-        BootChargingStationResult result = domainService.bootChargingStation(chargingStationId, parameters.getChargePointVendor(), parameters.getChargePointModel());
+        synchronized (continuation) {
+            if(continuation.isNew()) {
+                FutureTask futureResponse = new FutureTask<>(new Callable<BootNotificationResponse>() {
+                    @Override
+                    public BootNotificationResponse call() throws Exception {
+                        ChargingStationId chargingStationId = new ChargingStationId(chargeBoxIdentity);
 
-        BootNotificationResponse response = new BootNotificationResponse();
-        response.setStatus(result.isAccepted() ? RegistrationStatus.ACCEPTED : RegistrationStatus.REJECTED);
-        response.setHeartbeatInterval(result.getHeartbeatInterval());
-        response.setCurrentTime(DateConverter.toXmlGregorianCalendar(result.getTimeStamp()));
+                        chargingStationSubscriber.subscribe(chargingStationId);
 
-        return response;
+                        BootChargingStationResult result = domainService.bootChargingStation(chargingStationId, parameters.getChargePointVendor(), parameters.getChargePointModel());
+
+                        BootNotificationResponse response = new BootNotificationResponse();
+                        response.setStatus(result.isAccepted() ? RegistrationStatus.ACCEPTED : RegistrationStatus.REJECTED);
+                        response.setHeartbeatInterval(result.getHeartbeatInterval());
+                        response.setCurrentTime(DateConverter.toXmlGregorianCalendar(result.getTimeStamp()));
+
+                        // the blocking call has finished, we resume the transport thread
+                        continuation.resume();
+
+                        return response;
+                    }
+                });
+                taskExecutor.execute(futureResponse);
+                continuation.setObject(futureResponse);
+
+                // suspend the transport thread so it can handle other requests
+                continuation.suspend(CONTINUATION_TIMEOUT);
+                return null;
+            } else {
+                FutureTask futureTask = (FutureTask) continuation.getObject();
+                if(futureTask.isDone()) {
+                    try {
+                        return (BootNotificationResponse) futureTask.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        // handling the bootNotification has (probably) failed, log the error and return REJECTED
+
+                        log.error(e.getClass().getName() + " while waiting for response on boot notification, returning REJECTED to client.");
+                        e.printStackTrace();
+
+                        BootNotificationResponse response = new BootNotificationResponse();
+                        response.setHeartbeatInterval(HEARTBEAT_INTERVAL_FALLBACK);
+                        response.setStatus(RegistrationStatus.REJECTED);
+                        response.setCurrentTime(DateConverter.toXmlGregorianCalendar(new Date()));
+                        return response;
+                    }
+                } else {
+                    //TODO should the timeout decrease every time the task is not yet done? - Mark van den Bergh, November 21st 2013
+                    continuation.suspend(CONTINUATION_TIMEOUT);
+                }
+            }
+        }
+        // unreachable
+        return null;
     }
 
     @Override
     public HeartbeatResponse heartbeat(HeartbeatRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [heartbeat] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public MeterValuesResponse meterValues(MeterValuesRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [meterValues] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public DiagnosticsStatusNotificationResponse diagnosticsStatusNotification(DiagnosticsStatusNotificationRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [diagnosticsStatusNotification] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public AuthorizeResponse authorize(AuthorizeRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [authorize] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public FirmwareStatusNotificationResponse firmwareStatusNotification(FirmwareStatusNotificationRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [firmwareStatusNotification] called.");
         throw new RuntimeException("Not yet implemented");
     }
 
     @Override
     public StartTransactionResponse startTransaction(StartTransactionRequest parameters, String chargeBoxIdentity) {
         //FIXME implement me
+        log.error("Unimplemented method [startTransaction] called.");
         throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Gets a continuation object from the provider which is present in message context
+     *
+     * @return continuation
+     */
+    private Continuation getContinuation() {
+        ContinuationProvider provider = (ContinuationProvider) context.getMessageContext().get(ContinuationProvider.class.getName());
+        return provider.getContinuation();
     }
 
 }
