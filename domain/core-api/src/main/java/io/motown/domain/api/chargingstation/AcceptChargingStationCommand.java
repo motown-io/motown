0bd9079baf64f6e464eb434cefacf9e176eb41c2
Dennis Laumen <dennis.laumen@ihomer.nl>
2013-12-13 17:55:00 +0100

    Reformatted command

--- domain/core-api/src/main/java/io/motown/domain/api/chargingstation/AcceptChargingStationCommand.java
+++ domain/core-api/src/main/java/io/motown/domain/api/chargingstation/AcceptChargingStationCommand.java
@@ -24,6 +24,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * the charging infrastructure by an operator.
  */
 public final class AcceptChargingStationCommand {
+
     @TargetAggregateIdentifier
     private final ChargingStationId chargingStationId;
 
