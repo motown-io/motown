5432710f7e8f526ea329dc111b21029fd9bdff52
Mark van den Bergh <mark.van.den.bergh@ihomer.nl>
2014-01-06 11:10:45 +0100

    Added missing header

--- ocpp/view-model/src/test/java/io/motown/ocpp/viewmodel/domain/CreateChargingStationCommandCallbackTest.java
+++ ocpp/view-model/src/test/java/io/motown/ocpp/viewmodel/domain/CreateChargingStationCommandCallbackTest.java
@@ -20,7 +20,6 @@ import io.motown.ocpp.viewmodel.persistence.repostories.ChargingStationRepositor
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
