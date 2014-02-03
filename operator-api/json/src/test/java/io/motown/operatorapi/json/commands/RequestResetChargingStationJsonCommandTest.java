2f509e89f2f581d0f2ad0a5a54351ee48fb713c7
Mark Manders <mark.manders@ihomer.nl>
2014-02-03 14:36:35 +0100

    added requestResetChargingStation command test

---
+++ operator-api/json/src/test/java/io/motown/operatorapi/json/commands/RequestResetChargingStationJsonCommandTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2013 Motown.IO (info@motown.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.motown.operatorapi.json.commands;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RequestResetChargingStationJsonCommandTest {
+    private Gson gson;
+    private RequestResetChargingStationJsonCommandHandler handler = new RequestResetChargingStationJsonCommandHandler();
+
+    @Before
+    public void setUp() {
+        this.gson = OperatorApiJsonTestUtils.getGson();
+        handler.setGson(gson);
+        handler.setCommandGateway(new TestDomainCommandGateway());
+        handler.setRepository(OperatorApiJsonTestUtils.getMockChargingStationRepository());
+    }
+
+    @Test
+    public void testResetCommand() {
+        JsonObject commandObject = gson.fromJson("{type:'soft'}", JsonObject.class);
+        handler.handle("TEST_REGISTERED", commandObject);
+    }
+
+    @Test
+    public void testHardResetCommand() {
+        JsonObject commandObject = gson.fromJson("{type:'hard'}", JsonObject.class);
+        handler.handle("TEST_REGISTERED", commandObject);
+    }
+
+}
