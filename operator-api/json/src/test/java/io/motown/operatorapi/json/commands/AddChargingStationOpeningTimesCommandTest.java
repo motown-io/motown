bf65af2e93ab085cec34cdeb8f0ee7b9ad521d77
Mark Manders <mark.manders@ihomer.nl>
2014-02-14 15:37:17 +0100

    added tests for opening times

---
+++ operator-api/json/src/test/java/io/motown/operatorapi/json/commands/AddChargingStationOpeningTimesCommandTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2013 Motown.IO (info@motown.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.motown.operatorapi.json.commands;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddChargingStationOpeningTimesCommandTest {
+    private Gson gson;
+    private AddChargingStationOpeningTimesJsonCommandHandler handler = new AddChargingStationOpeningTimesJsonCommandHandler();
+
+    @Before
+    public void setUp() {
+        gson = OperatorApiJsonTestUtils.getGson();
+        handler.setGson(gson);
+        handler.setCommandGateway(new TestDomainCommandGateway());
+        handler.setRepository(OperatorApiJsonTestUtils.getMockChargingStationRepository());
+    }
+
+    @Test
+    public void testSetOpeningTimes() {
+        JsonObject commandObject = gson.fromJson("{openingTimes:[{day:1,timeStart:200,timeStop:400}]}", JsonObject.class);
+        handler.handle(OperatorApiJsonTestUtils.CHARGING_STATION_ID_STRING, commandObject);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetOpeningTimesInvalidDay() {
+        JsonObject commandObject = gson.fromJson("{openingTimes:[{day:8,timeStart:200,timeStop:400}]}", JsonObject.class);
+        handler.handle(OperatorApiJsonTestUtils.CHARGING_STATION_ID_STRING, commandObject);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testSetOpeningTimesInvalidArgument() {
+        JsonObject commandObject = gson.fromJson("{openingTimes:[{day:'MONDAY',timeStart:200,timeStop:400}]}", JsonObject.class);
+        handler.handle(OperatorApiJsonTestUtils.CHARGING_STATION_ID_STRING, commandObject);
+    }
+}
