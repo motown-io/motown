3d2e7d85a319b4de128886a183b03c2f1a5266f6
Mark Manders <mark.manders@ihomer.nl>
2014-02-17 14:19:08 +0100

    added package-info.java for a global comment

---
+++ operator-api/json/src/main/java/io/motown/operatorapi/json/commands/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains all JSON command handlers in the operator-api. Each command has its own class and handles its own specific command.
+ */
+package io.motown.operatorapi.json.commands;
