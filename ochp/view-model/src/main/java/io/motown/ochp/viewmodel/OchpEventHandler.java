e703c153e9af2d6fedd326b72a5ec97bba8fec86
Ingo Pak <ingo.pak@ihomer.nl>
2014-03-05 11:29:42 +0100

    Initial version of OCHP add-on

---
+++ ochp/view-model/src/main/java/io/motown/ochp/viewmodel/OchpEventHandler.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2013 Motown.IO (info@motown.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.motown.ochp.viewmodel;
+
+import io.motown.domain.api.chargingstation.ChargingStationAcceptedEvent;
+import io.motown.domain.api.chargingstation.ChargingStationConfiguredEvent;
+import io.motown.domain.api.chargingstation.ChargingStationCreatedEvent;
+import io.motown.ochp.viewmodel.persistence.entities.ChargingStation;
+import io.motown.ochp.viewmodel.persistence.repostories.ChargingStationRepository;
+import org.axonframework.eventhandling.annotation.EventHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OchpEventHandler {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(io.motown.ochp.viewmodel.OchpEventHandler.class);
+
+    @Autowired
+    private ChargingStationRepository chargingStationRepository;
+
+    //TODO: Add eventhandlers for keeping internal OCHP state up to date - Ingo Pak, 05 Mar 2014
+    
+    @EventHandler
+    public void handle(ChargingStationCreatedEvent event) {
+        LOG.info("Handling ChargingStationCreatedEvent");
+        String chargingStationId = event.getChargingStationId().getId();
+        ChargingStation chargingStation = chargingStationRepository.findOne(chargingStationId);
+
+        if (chargingStation == null) {
+            chargingStation = new ChargingStation(chargingStationId);
+        }
+        chargingStationRepository.save(chargingStation);
+    }
+
+    @EventHandler
+    public void handle(ChargingStationAcceptedEvent event) {
+        LOG.debug("ChargingStationAcceptedEvent for [{}] received!", event.getChargingStationId());
+
+        ChargingStation chargingStation = chargingStationRepository.findOne(event.getChargingStationId().getId());
+
+        if (chargingStation != null) {
+            chargingStation.setRegistered(true);
+            chargingStationRepository.save(chargingStation);
+        } else {
+            LOG.error("OCHP module repo COULD NOT FIND CHARGEPOINT {} and mark it as registered", event.getChargingStationId());
+        }
+    }
+
+    @EventHandler
+    public void handle(ChargingStationConfiguredEvent event) {
+        LOG.info("ChargingStationConfiguredEvent");
+
+        String chargingStationId = event.getChargingStationId().getId();
+        ChargingStation chargingStation = chargingStationRepository.findOne(chargingStationId);
+
+        if (chargingStation == null) {
+            LOG.warn("Received a ChargingStationConfiguredEvent for unknown charging station. Creating the chargingStation.");
+            chargingStation = new ChargingStation(chargingStationId);
+        }
+
+        chargingStation.setNumberOfEvses(event.getEvses().size());
+        chargingStation.setConfigured(true);
+
+        chargingStationRepository.save(chargingStation);
+    }
+
+    public void setChargingStationRepository(ChargingStationRepository chargingStationRepository) {
+        this.chargingStationRepository = chargingStationRepository;
+    }
+}
---
+++ ochp/view-model/src/main/java/io/motown/ochp/viewmodel/persistence/entities/Transaction.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2013 Motown.IO (info@motown.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.motown.ochp.viewmodel.persistence.entities;
+
+import io.motown.domain.api.chargingstation.EvseId;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String evseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public EvseId getEvseId() {
+        return new EvseId(Integer.valueOf(evseId));
+    }
+
+    public void setEvseId(EvseId evseId) {
+        this.evseId = evseId.getId();
+    }
+
+}
