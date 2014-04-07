f51c8b574da18a3364ae653952f37f23201b6849
Ingo Pak <ingo.pak@ihomer.nl>
2014-04-07 15:58:14 +0200

    OCPPJ refactored get diagnostics handler test

--- ocpp/websocket-json/src/test/java/io.motown.ocpp.websocketjson/response/handler/GetDiagnosticsResponseHandlerTest.java
+++ ocpp/websocket-json/src/test/java/io.motown.ocpp.websocketjson/response/handler/GetDiagnosticsResponseHandlerTest.java
@@ -20,12 +20,11 @@ import io.motown.domain.api.chargingstation.ChargingStationId;
 import io.motown.domain.api.chargingstation.CorrelationToken;
 import io.motown.domain.api.security.AddOnIdentity;
 import io.motown.ocpp.viewmodel.domain.DomainService;
+import io.motown.ocpp.websocketjson.schema.generated.v15.GetdiagnosticsResponse;
 import io.motown.ocpp.websocketjson.wamp.WampMessage;
-import io.motown.ocpp.websocketjson.wamp.WampMessageParser;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.StringReader;
 import java.util.UUID;
 
 import static io.motown.domain.api.chargingstation.test.ChargingStationTestUtils.CHARGING_STATION_ID;
@@ -41,23 +40,26 @@ public class GetDiagnosticsResponseHandlerTest {
 
     private DomainService domainService;
 
+    private String token;
+    private CorrelationToken correlationToken;
+    private GetDiagnosticsResponseHandler handler;
+
     @Before
     public void setup() {
         gson = getGson();
         domainService = mock(DomainService.class);
+
+        token = UUID.randomUUID().toString();
+        correlationToken = new CorrelationToken(token);
+        handler = new GetDiagnosticsResponseHandler(correlationToken);
     }
 
     @Test
     public void handleValidResponse() {
-        String token = UUID.randomUUID().toString();
-        CorrelationToken correlationToken = new CorrelationToken(token);
-        GetDiagnosticsResponseHandler handler = new GetDiagnosticsResponseHandler(correlationToken);
+        GetdiagnosticsResponse payload = new GetdiagnosticsResponse();
+        payload.setFileName("diag-gir.vat.mx.000e48-20130131132608.txt");
+        WampMessage message = new WampMessage(WampMessage.CALL_RESULT, token, gson.toJson(payload));
 
-        String responseMessage = "[%d,\"%s\",{\n" +
-                "  \"fileName\": \"diag-gir.vat.mx.000e48-20130131132608.txt\"\n" +
-                "}]";
-
-        WampMessage message = new WampMessageParser(gson).parseMessage(new StringReader(String.format(responseMessage, WampMessage.CALL_RESULT, token)));
         handler.handle(CHARGING_STATION_ID, message, gson, domainService, null);
 
         verify(domainService).diagnosticsFileNameReceived(any(ChargingStationId.class), anyString(), any(CorrelationToken.class), any(AddOnIdentity.class));
