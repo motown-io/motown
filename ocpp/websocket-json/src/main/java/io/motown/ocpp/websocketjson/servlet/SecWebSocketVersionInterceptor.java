6b9c34baf3060e40077d5489a7199d3127c7e3b9
Dennis Laumen <dennis.laumen@ihomer.nl>
2014-04-19 13:37:17 +0200

    Added doc to empty method

--- ocpp/websocket-json/src/main/java/io/motown/ocpp/websocketjson/servlet/SecWebSocketVersionInterceptor.java
+++ ocpp/websocket-json/src/main/java/io/motown/ocpp/websocketjson/servlet/SecWebSocketVersionInterceptor.java
@@ -19,12 +19,7 @@ import org.atmosphere.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -79,6 +74,8 @@ public class SecWebSocketVersionInterceptor implements Filter {
 
     @Override
     public void destroy() {
+        // We are forced to implement this method from the Filter interface but we currently do not require any
+        // clean-up.
     }
 
 }
