28674d76617543827bb44a61dd1217b1cdce8910
Mark van den Bergh <mark.van.den.bergh@ihomer.nl>
2014-04-22 15:36:06 +0200

    Authorization in RequestCancelReservationJsonCommandHandler

--- operator-api/json/src/main/java/io/motown/operatorapi/json/commands/RequestCancelReservationJsonCommandHandler.java
+++ operator-api/json/src/main/java/io/motown/operatorapi/json/commands/RequestCancelReservationJsonCommandHandler.java
@@ -22,6 +22,8 @@ import io.motown.domain.api.chargingstation.ChargingStationId;
 import io.motown.domain.api.chargingstation.CorrelationToken;
 import io.motown.domain.api.chargingstation.RequestCancelReservationCommand;
 import io.motown.domain.api.security.IdentityContext;
+import io.motown.domain.commandauthorization.CommandAuthorizationService;
+import io.motown.operatorapi.json.exceptions.UserIdentityUnauthorizedException;
 import io.motown.operatorapi.viewmodel.model.RequestCancelReservationApiCommand;
 import io.motown.operatorapi.viewmodel.persistence.entities.ChargingStation;
 import io.motown.operatorapi.viewmodel.persistence.repositories.ChargingStationRepository;
@@ -36,20 +38,34 @@ class RequestCancelReservationJsonCommandHandler implements JsonCommandHandler {
 
     private Gson gson;
 
+    private CommandAuthorizationService commandAuthorizationService;
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getCommandName() {
         return COMMAND_NAME;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void handle(String chargingStationId, JsonObject commandObject, IdentityContext identityContext) {
+    public void handle(String chargingStationId, JsonObject commandObject, IdentityContext identityContext) throws UserIdentityUnauthorizedException {
+        ChargingStationId csId = new ChargingStationId(chargingStationId);
+
+        if (!commandAuthorizationService.isAuthorized(csId, identityContext.getUserIdentity(), RequestCancelReservationCommand.class)) {
+            throw new UserIdentityUnauthorizedException(chargingStationId, identityContext.getUserIdentity(), RequestCancelReservationCommand.class);
+        }
+
         try {
             ChargingStation chargingStation = repository.findOne(chargingStationId);
             if (chargingStation != null && chargingStation.isAccepted()) {
 
                 RequestCancelReservationApiCommand command = gson.fromJson(commandObject, RequestCancelReservationApiCommand.class);
 
-                commandGateway.send(new RequestCancelReservationCommand(new ChargingStationId(chargingStationId), command.getReservationId(), identityContext), new CorrelationToken());
+                commandGateway.send(new RequestCancelReservationCommand(csId, command.getReservationId(), identityContext), new CorrelationToken());
             } else {
                 throw new IllegalStateException("It is not possible to cancel a reservation on a charging station that is not registered");
             }
@@ -58,15 +74,40 @@ class RequestCancelReservationJsonCommandHandler implements JsonCommandHandler {
         }
     }
 
+    /**
+     * Sets the command gateway.
+     *
+     * @param commandGateway the command gateway.
+     */
     public void setCommandGateway(DomainCommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
 
+    /**
+     * Sets the charging station repository.
+     *
+     * @param repository the charging station repository.
+     */
     public void setRepository(ChargingStationRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Sets the GSON instance.
+     *
+     * @param gson the GSON instance.
+     */
     public void setGson(Gson gson) {
         this.gson = gson;
+    }
+
+    /**
+     * Sets the command authorization service to use. The command authorization service checks if a certain user is
+     * allowed to execute a certain command.
+     *
+     * @param commandAuthorizationService command authorization.
+     */
+    public void setCommandAuthorizationService(CommandAuthorizationService commandAuthorizationService) {
+        this.commandAuthorizationService = commandAuthorizationService;
     }
 }
