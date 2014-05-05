0a1a4e1d98fce1008978123cc87731f786f3210e
Ingo Pak <ingo.pak@ihomer.nl>
2014-05-05 14:49:39 +0200

    Added interface for SoapHeaderReader with specific CXF implementation

--- ocpp/soap-utils/src/main/java/io/motown/ocpp/soaputils/header/SoapHeaderReader.java
+++ ocpp/soap-utils/src/main/java/io/motown/ocpp/soaputils/header/SoapHeaderReader.java
@@ -15,46 +15,9 @@
  */
 package io.motown.ocpp.soaputils.header;
 
-import org.apache.cxf.headers.Header;
-import org.apache.cxf.helpers.CastUtils;
-import org.apache.cxf.jaxws.context.WrappedMessageContext;
-import org.apache.cxf.message.Message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.w3c.dom.Element;
-
 import javax.xml.ws.handler.MessageContext;
-import java.util.List;
 
-public class SoapHeaderReader {
+public interface SoapHeaderReader {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SoapHeaderReader.class);
-
-    /**
-     * Gets the charging station address from the SOAP "From" header.
-     *
-     * @param messageContext message context
-     * @return charging station address, or empty string if From header is empty or doesn't exist.
-     */
-    public String getChargingStationAddress(MessageContext messageContext) {
-        if (!(messageContext instanceof WrappedMessageContext)) {
-            LOG.warn("Unable to get message context, or message context is not the right type.");
-            return "";
-        }
-
-        Message message = ((WrappedMessageContext) messageContext).getWrappedMessage();
-        List<Header> headers = CastUtils.cast((List<?>) message.get(Header.HEADER_LIST));
-
-        for (Header h : headers) {
-            Element n = (Element) h.getObject();
-
-            if ("From".equals(n.getLocalName())) {
-                return n.getTextContent();
-            }
-        }
-
-        LOG.warn("No 'From' header found in request. Not able to determine charging station address.");
-        return "";
-    }
-
+    String getChargingStationAddress(MessageContext messageContext);
 }
