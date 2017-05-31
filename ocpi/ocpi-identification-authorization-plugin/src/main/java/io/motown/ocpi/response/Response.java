d639576148acf1eb07de2b6a2a7d6e23f41f83bc
bartwolfs <bart.wolfs@ihomer.nl>
2017-05-31 15:14:49 +0200

    OCPI Authorization - Review Comment Processed

--- ocpi/ocpi-identification-authorization-plugin/src/main/java/io/motown/ocpi/response/Response.java
+++ ocpi/ocpi-identification-authorization-plugin/src/main/java/io/motown/ocpi/response/Response.java
@@ -33,53 +33,54 @@ public class Response {
 
 	public enum StatusCode {
 
-	    SUCCESS(1000),
-	    CLIENT_ERROR(2000),
-	    INVALID_OR_MISSING_PARAMETERS(2001),
-	    GENERIC_SERVER_ERROR(3000),
-	    UNABLE_TO_USE_CLIENT_API(3001),
-	    UNSUPPORTED_VERSION(3002);
+		SUCCESS(1000), CLIENT_ERROR(2000), INVALID_OR_MISSING_PARAMETERS(2001), GENERIC_SERVER_ERROR(
+				3000), UNABLE_TO_USE_CLIENT_API(3001), UNSUPPORTED_VERSION(3002);
 
-	    /**
-	     * Return the enum constant of this type with the specified numeric value.
-	     * @param statusCode the numeric value of the enum to be returned
-	     * @return the enum constant with the specified numeric value
-	     * @throws IllegalArgumentException if this enum has no constant for the specified numeric value
-	     */
-	    public static StatusCode valueOf(int statusCode) {
-	        for (StatusCode status : values()) {
-	            if (status.value == statusCode) {
-	                return status;
-	            }
-	        }
-	        throw new IllegalArgumentException("No matching constant for [" + statusCode + "]");
-	    }
+		/**
+		 * Return the enum constant of this type with the specified numeric
+		 * value.
+		 * 
+		 * @param statusCode
+		 *            the numeric value of the enum to be returned
+		 * @return the enum constant with the specified numeric value
+		 * @throws IllegalArgumentException
+		 *             if this enum has no constant for the specified numeric
+		 *             value
+		 */
+		public static StatusCode valueOf(int statusCode) {
+			for (StatusCode status : values()) {
+				if (status.value == statusCode) {
+					return status;
+				}
+			}
+			throw new IllegalArgumentException("No matching constant for [" + statusCode + "]");
+		}
 
-	    private final int value;
+		private final int value;
 
-	    private StatusCode(int value) {
-	        this.value = value;
-	    }
+		private StatusCode(int value) {
+			this.value = value;
+		}
 
-	    /**
-	     * Return a string representation of this status code.
-	     */
-	    @Override
-	    public String toString() {
-	        return Integer.toString(value);
-	    }
+		/**
+		 * Return a string representation of this status code.
+		 */
+		@Override
+		public String toString() {
+			return Integer.toString(value);
+		}
 
-	    /**
-	     * Return the integer value of this status code.
-	     */
-	    public int value() {
-	        return this.value;
-	    }
+		/**
+		 * Return the integer value of this status code.
+		 */
+		public int value() {
+			return this.value;
+		}
 
-	    public boolean equals(Integer code){
-	    	return this.value == code;
-	    }
-	    
+		public boolean equals(Integer code) {
+			return this.value == code;
+		}
+
 	}
 
 	@JsonIgnore
@@ -88,22 +89,25 @@ public class Response {
 	public Integer status_code;
 
 	public String status_message;
-	
+
 	public String timestamp;
 
 	public Response() {
-		
+
 	}
-	public Response(StatusCode status){
+
+	public Response(StatusCode status) {
 		this(status, status.name());
 	}
-	public Response(StatusCode status, String message)	{
+
+	public Response(StatusCode status, String message) {
 		this.status_code = status.value;
 		this.status_message = message;
 		this.timestamp = AppConfig.DATE_FORMAT.format(new Timestamp(System.currentTimeMillis()));
 	}
-	
-	public String toString(){
-		return timestamp + " " + this.getClass().getName() + "; status: " + status_code + "; message: " + status_message; 
+
+	public String toString() {
+		return timestamp + " " + this.getClass().getName() + "; status: " + status_code + "; message: "
+				+ status_message;
 	}
 }
