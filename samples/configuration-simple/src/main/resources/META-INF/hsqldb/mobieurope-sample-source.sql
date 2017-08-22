2f115ac71e66547abb935c95c42c6870e6dc3a25
Mark van den Bergh <mark.van.den.bergh@ihomer.nl>
2017-08-22 11:25:58 +0200

    Fix endpoint definition

--- samples/configuration-simple/src/main/resources/META-INF/hsqldb/mobieurope-sample-source.sql
+++ samples/configuration-simple/src/main/resources/META-INF/hsqldb/mobieurope-sample-source.sql
@@ -5,4 +5,4 @@ INSERT INTO DESTINATIONENDPOINT (PMSIDENTIFIER, DESTINATIONENDPOINTURL)
 VALUES ('IE-001', 'http://localhost:8089/services/mobiEuropeDestinationService');
 
 INSERT INTO DESTINATIONENDPOINT (PMSIDENTIFIER, DESTINATIONENDPOINTURL)
-VALUES ('NL-AMS', 'https://lukas-ams.com/mobieurope/services/mobieurope/pms/destination/v1');
+VALUES ('NL-OTH', 'https://otherbackoffice.com/mobieurope/services/mobieurope/pms/destination/v1');
